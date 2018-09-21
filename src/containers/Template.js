fc12ac92fe8a16b9c9dd2a78f7077c0fc7da79df
Mo Naggar <moses@Mos-MacBook-Air.local>
2018-09-21 15:54:25 -0400

    green box added

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -5,12 +5,39 @@ import { Route } from 'react-router-dom';
 import Profile from './Profile'
 import Home from './Home'
 import { Header, Main } from '../styled/Template'
+import { Stage, Layer, Text, Rect } from 'react-konva';
+import Konva from 'konva';
+
+function insideBox() {
+	console.log("ouii");
+}
 
 class Template extends Component {
+
+	state = {
+		rows: 3
+	}
+
+	componentWillMount() {
+		let height = window.innerHeight
+		let width = window.innerWidth
+		let size = (height > width) ? width * 0.8 : height * 0.8
+		let rows = this.state.rows
+		let units = size / rows
+
+		this.setState({
+			size: size,
+			rows: rows,
+			units: units
+		})
+	}
 
 	render() {
 		return (
 			<div>
+
+
+
 				<MuiThemeProvider>
 					<div>
 						<MenuDrawer />
@@ -25,7 +52,17 @@ class Template extends Component {
 
 					</div>
 				</MuiThemeProvider>
-
+				
+				<Stage onMouseEnter={insideBox}
+					width={this.state.units}
+					height={this.state.units}>
+					<Layer>
+						<Rect fill='green'
+							width={this.state.units}
+							height={this.state.units} />
+						<Text text="Finally?YESSSS" />
+					</Layer>
+				</Stage>
 				<Route path="/Profile" component={Profile} />
 				<Route path="/Home" component={Home} />
 
